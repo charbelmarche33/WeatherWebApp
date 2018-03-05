c92395d3f96ee40d16e5844d1000f0e2341131e5
Charbel Marche <cmarche@mail.umw.edu>
2018-03-05 23:10:08 +0000

    Added the bootstrap template and made slight changes. Will continue to work later.

---
+++ README.md
@@ -0,0 +1,6 @@
+To get this running do these commands in terminal:
+__________________________________________________
+
+- sudo apt-get update
+- sudo apt-get install python-psycopg2
+- sudo apt-get install flask
---
+++ WeatherApp/server.py
@@ -0,0 +1,23 @@
+import os, time, os.path, psycopg2, psycopg2.extras
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+password = False
+
+#def connectToDB():
+#    connectionString = 'dbname=contacts user=cuser password=123abc host=localhost'
+#    try:
+#        return psycopg2.connect(connectionString)
+#    except:
+#        print("Cannot connect to DB")
+        
+        
+@app.route('/')
+def mainIndex(): 
+    return render_template('index.html')
+    
+    
+#Start the server here
+if __name__ == '__main__':
+    app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug = True)
