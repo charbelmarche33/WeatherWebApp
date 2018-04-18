00be4b7fb7e8b4408fb691dc999b2df32cc86500
rmooney <rmooney@mail.umw.edu>
2018-04-18 12:53:51 -0400

    working on stuff

--- server.py
+++ server.py
@@ -39,16 +39,16 @@ def connectToDB():
         
 @application.route('/pastsearches', methods=['GET', 'POST'])
 def pastSearches():
-    if(session.get('username') == ''):
+    if(session['username'] == ''):
         #User is not logged in, redirect to main page
         return redirect(url_for('mainIndex'))
     conn = connectToDB()
     curr = conn.cursor()
-    print(curr.mogrify("SELECT * FROM pastsearches where username = %s;", (session.get('username'), )))
-    curr.execute("SELECT * FROM pastsearches where username = %s;", (session.get('username'), ))
+    print(curr.mogrify("SELECT * FROM pastsearches where username = %s;", (session['username'], )))
+    curr.execute("SELECT * FROM pastsearches where username = %s;", (session['username'], ))
     results = curr.fetchall()
     results.reverse()
-    return render_template('pastsearches.html', username=session.get('username'), results=results)
+    return render_template('pastsearches.html', username=session['username'], results=results)
     
     
 @application.route('/signout', methods=['GET', 'POST'])
@@ -63,7 +63,7 @@ def mainIndex():
     #If the user name hasn't been created yet then set to '' otherwise someone is logged in dont
     #The try will break and make you go to the except where you set username to ''
     try:
-        print('User: ' + session.get('username'))
+        print('User: ' + session['username'])
     except:
         #Session ensures that the logged in status remains across tabs and clicks. It is a variable stored in the cache of the browser being used!
         #ALSO NOTE that username is used in the index file as a way to tell if anyone is logged in!
@@ -629,10 +629,10 @@ def mainIndex():
         getLocation = getLocation.replace("",'')
         
     #This is going to add to the past searches
-    if (session.get('username') != '' and getLocation != '' and validLocation and validDate):
+    if (session['username'] != '' and getLocation != '' and validLocation and validDate):
         print(getLocation)
         print(date)
-        print(session.get('username'))
+        print(session['username'])
         query = curr.mogrify("INSERT into pastsearches (username, locationSearched, dateSearched) VALUES (%s,%s,%s);", (session['username'], getLocation, date))
         print(query)
         curr.execute(query)
@@ -640,8 +640,7 @@ def mainIndex():
         curr.execute("SELECT * FROM pastsearches;")
         res = curr.fetchall()
         print(res)
-	print("this is .get " + session.get('username') + " this is the regular " + session['username']) 
-    return render_template('index.html', username=session.get('username'), validSignUpCredentials=validSignUpCredentials, 
+    return render_template('index.html', username=session['username'], validSignUpCredentials=validSignUpCredentials, 
                                         validLogInCredentials=validLogInCredentials, results=results, date=date, unixDate = unixDate, 
                                         todaysDate=todaysDate, TodaysDate=TodaysDate, weekdayName=weekdayName, average_temperature=average_temperature, lowTemp=lowTemp, 
                                         highTemp=highTemp, precip=precip, precipType=precipType, currentTemp=currentTemp, wind=wind, humidity=humidity, 
